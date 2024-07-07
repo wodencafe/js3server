9bcd79aded5d649dec5ef316a6f51ab848bae890
wodencafe <wodencafe@gmail.com>
2024-07-07 16:51:34 -0500

    chore: Initial project setup

---
+++ src/main/kotlin/chat/llamas/js3server/Js3serverApplication.kt
@@ -0,0 +1,11 @@
+package chat.llamas.js3server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Js3serverApplication
+
+fun main(args: Array<String>) {
+	runApplication<Js3serverApplication>(*args)
+}
---
+++ src/test/kotlin/chat/llamas/js3server/Js3serverApplicationTests.kt
@@ -0,0 +1,13 @@
+package chat.llamas.js3server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Js3serverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
